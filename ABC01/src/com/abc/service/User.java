0b6bd91a08562f5ce73cc70fda508a4114e550b7
achen-rzf <allen.chen@razorfish.com>
2017-11-13 21:59:14 -0600

    Update User.java

--- ABC01/src/com/abc/service/User.java
+++ ABC01/src/com/abc/service/User.java
@@ -4,10 +4,7 @@ public class User {
 	
 	public boolean login(String userName,String password){
 		boolean result = false;
-<<<<<<< .merge_file_S6J90n
 		System.out.println("01");
-=======
->>>>>>> .merge_file_fKqkxo
 		System.out.println("login ok!");
 		return result;
 	}
